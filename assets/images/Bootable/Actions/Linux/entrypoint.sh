92482d92baff540877a0e9354a240efd5b5e7469
eq19 <admin@eq19.com>
2023-06-21 10:54:56 +0700

    Update entrypoint.sh

--- assets/images/Bootable/Actions/Linux/entrypoint.sh
+++ assets/images/Bootable/Actions/Linux/entrypoint.sh
@@ -21,9 +21,9 @@ deploy_remote() {
 
 # pinned repos
 # https://stackoverflow.com/a/43358500/4058484
-echo -e "\n$hr\nPINNED  REPOSITORIES\n$hr"
+echo -e "\n$hr\nCONFIG FILE\n$hr"
 sed -i "1s|^|repository: $GITHUB_REPOSITORY\n|" ${JEKYLL_CFG}
-curl -s -X POST "${GITHUB_GRAPHQL_URL}" -H "Authorization: bearer $TOKEN" --data-raw '{"query":"{\n  user(login: \"'${GITHUB_REPOSITORY_OWNER}'\") {\n pinnedItems(first: 6, types: REPOSITORY) {\n nodes {\n ... on Repository {\n name\n }\n }\n }\n }\n}"' | jq --raw-output '.data.user.pinnedItems' | yq eval -P >> ${JEKYLL_CFG} && cat ${JEKYLL_CFG}
+curl -s -X POST "${GITHUB_GRAPHQL_URL}" -H "Authorization: bearer $TOKEN" --data-raw '{"query":"{\n  user(login: \"'${GITHUB_REPOSITORY_OWNER}'\") {\n pinnedItems(first: 6, types: REPOSITORY) {\n nodes {\n ... on Repository {\n name\n }\n }\n }\n }\n}"' | jq --raw-output '.data.user.pinnedItems' | yq eval -P | sed -e "s/name: //2" >> ${JEKYLL_CFG} && cat ${JEKYLL_CFG}
 
 echo -e "\n$hr\nJEKYLL BUILD\n$hr" && pwd
 # https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
