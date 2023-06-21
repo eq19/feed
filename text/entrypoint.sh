d4a1506fa409d25a76caf629e44baa1988ff8a61
eq19 <admin@eq19.com>
2023-06-22 03:31:05 +0700

    Update entrypoint.sh

--- text/entrypoint.sh
+++ text/entrypoint.sh
@@ -16,6 +16,7 @@ echo -e "\n$hr\nJEKYLL BUILD\n$hr"
 JEKYLL_GITHUB_TOKEN=${TOKEN} bundle exec jekyll build --trace --profile \
   ${JEKYLL_BASEURL} -c ${JEKYLL_CFG}
 
+echo -e "\n$hr\nDEPLOY\n$hr"
 cd ./_site #&& touch .nojekyll
 git config --global user.name "${ACTOR}"
 git config --global user.email "${ACTOR}@users.noreply.github.com"
