32c0b49db6b459b5d61685f5144c9d506c640b20
eq19 <admin@eq19.com>
2023-06-21 09:45:14 +0700

    Update entrypoint.sh

--- assets/images/Bootable/Actions/Linux/entrypoint.sh
+++ assets/images/Bootable/Actions/Linux/entrypoint.sh
@@ -22,7 +22,7 @@ deploy_remote() {
 # pinned repos
 # https://stackoverflow.com/a/43358500/4058484
 echo -e "\n$hr\nPINNED  REPOSITORIES\n$hr"
-curl -s -X POST "${GITHUB_GRAPHQL_URL}" -H "Authorization: bearer $TOKEN" --data-raw '{"query":"{\n  user(login: \"'${GITHUB_REPOSITORY_OWNER}'\") {\n pinnedItems(first: 6, types: REPOSITORY) {\n nodes {\n ... on Repository {\n name\n }\n }\n }\n }\n}"' | jq --raw-output '.data.user.pinnedItems' | yq -P > ${JEKYLL_CFG}
+curl -s -X POST "${GITHUB_GRAPHQL_URL}" -H "Authorization: bearer $TOKEN" --data-raw '{"query":"{\n  user(login: \"'${GITHUB_REPOSITORY_OWNER}'\") {\n pinnedItems(first: 6, types: REPOSITORY) {\n nodes {\n ... on Repository {\n name\n }\n }\n }\n }\n}"' | jq --raw-output '.data.user.pinnedItems' | yq -r 'toYaml' > ${JEKYLL_CFG}
 
 echo -e "\n$hr\nJEKYLL BUILD\n$hr" && pwd
 # https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
