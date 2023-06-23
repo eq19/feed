d6684d750ff21b33957e0baa604ec36257431d8f
eq19 <admin@eq19.com>
2023-06-23 09:45:42 +0700

    Update remote.sh

--- text/scripts/remote.sh
+++ text/scripts/remote.sh
@@ -13,8 +13,4 @@ deploy_remote() {
   mv -v ${GITHUB_WORKSPACE}/_site/* . && git add .
   git commit -m "jekyll build from cction ${GITHUB_SHA}"
   git push -u origin gh-pages
-
-  #git remote add origin ${REMOTE_REPO} && git fetch &>/dev/null
-  #git add . && git commit -m "jekyll build from Action ${GITHUB_SHA}"
-  #git push --force --quiet ${REMOTE_REPO} master:${BRANCH}
 }
