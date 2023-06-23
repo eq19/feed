f9bb20ec52d6c238c581163303940e090f5acf4b
eq19 <admin@eq19.com>
2023-06-23 15:18:17 +0700

    Update entrypoint.sh

--- text/entrypoint.sh
+++ text/entrypoint.sh
@@ -8,9 +8,9 @@ JEKYLL_CFG=${GITHUB_WORKSPACE}/_config.yml
 
 echo -e "\n$hr\nCONFIG FILE\n$hr"
 mv /maps/_config.yml ${JEKYLL_CFG}
-source ${SCRIPT_DIR}/config.sh && setup_config && cat ${JEKYLL_CFG} && ls -al
+source ${SCRIPT_DIR}/config.sh && setup_config && cat ${JEKYLL_CFG}
 
-echo -e "\n$hr\nJEKYLL BUILD\n$hr"
+echo -e "\n$hr\nJEKYLL BUILD\n$hr" && ls -al
 # https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
 JEKYLL_GITHUB_TOKEN=${TOKEN} bundle exec jekyll build --trace --profile \
   ${JEKYLL_BASEURL} -c ${JEKYLL_CFG}
