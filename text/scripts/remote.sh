1c6e69f5dc1e6fb19cbd62be8f56dfd5e1dacb56
eq19 <admin@eq19.com>
2023-06-23 09:13:55 +0700

    Update remote.sh

--- text/scripts/remote.sh
+++ text/scripts/remote.sh
@@ -4,7 +4,16 @@ deploy_remote() {
   echo -e "Deploying to $1 on branch ${BRANCH}"
   REMOTE_REPO="https://${ACTOR}:${TOKEN}@github.com/$1.git"
 
-  git remote add origin ${REMOTE_REPO} && git fetch &>/dev/null
-  git add . && git commit -m "jekyll build from Action ${GITHUB_SHA}"
-  git push --force --quiet ${REMOTE_REPO} master:${BRANCH}
+  git config --global user.name "${ACTOR}"
+  git config --global user.email "${ACTOR}@users.noreply.github.com"
+
+  git clone -b gh-pages --single-branch ${REMOTE_REPO} &>/dev/null
+  cd "$(basename "${REMOTE_REPO}" .git)" && rm -rf .
+  mv ${GITHUB_WORKSPACE}/_site/* . && git add .
+  git commit --allow-empty -m "jekyll build from cction ${GITHUB_SHA}"
+  git push -u origin gh-pages
+
+  #git remote add origin ${REMOTE_REPO} && git fetch &>/dev/null
+  #git add . && git commit -m "jekyll build from Action ${GITHUB_SHA}"
+  #git push --force --quiet ${REMOTE_REPO} master:${BRANCH}
 }
