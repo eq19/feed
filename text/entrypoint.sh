d76140bec2b14f8246304b9dc9b1d03237ead6f0
eq19 <admin@eq19.com>
2023-06-24 11:50:50 +0700

    Update entrypoint.sh

--- text/entrypoint.sh
+++ text/entrypoint.sh
@@ -17,7 +17,7 @@ JEKYLL_GITHUB_TOKEN=${TOKEN} bundle exec jekyll build --trace --profile \
   ${JEKYLL_BASEURL} -c ${JEKYLL_CFG}
 
 echo -e "\n$hr\nDEPLOY\n$hr"
-cd ./_site #&& touch .nojekyll
+cd ./_site && touch .nojekyll
 git config --global user.name "${ACTOR}"
 git config --global user.email "${ACTOR}@users.noreply.github.com"
 git init --initial-branch=master && source ${SCRIPT_DIR}/remote.sh && deploy_remote "${REPOSITORY}"
