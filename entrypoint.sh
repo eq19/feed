d89956e261d32f77b5027c54e71cb08cf3c1f951
eq19 <admin@eq19.com>
2023-06-25 21:12:03 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,29 +7,23 @@ deploy_remote() {
   git config --global user.name "${GITHUB_ACTOR}" && git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
   git clone -b gh-pages --single-branch ${REMOTE_REPO} &>/dev/null && cd $(basename $1) && rm -rf *
   mv -v ${GITHUB_WORKSPACE}/_site/* . && touch .nojekyll && git add .
-  git commit -m "jekyll build from cction ${GITHUB_SHA}"
-  git push -u origin gh-pages
+  git commit -m "jekyll build" && git push -u origin gh-pages
 }
 
 jekyll_build() {
-  echo ${INPUT_TOKEN} | gh auth login --with-token
-  gh api -H "Accept: application/vnd.github+json" /user/orgs --jq "map(.login)" > nodes.json
-  curl -s -X POST "${GITHUB_GRAPHQL_URL}" -H "Authorization: bearer $INPUT_TOKEN" --data-raw '{"query":"{\n  user(login: \"'${GITHUB_REPOSITORY_OWNER}'\") {\n pinnedItems(first: 6, types: REPOSITORY) {\n nodes {\n ... on Repository {\n name\n }\n }\n }\n }\n}"' | jq --raw-output '.data.user.pinnedItems' | yq eval -P | sed "s/name: //g" > nodes.yaml
+  curl -s -X POST "${GITHUB_GRAPHQL_URL}" -H "Authorization: bearer $INPUT_TOKEN" --data-raw '{"query":"{\n  user(login: \"'${OWNER}'\") {\n pinnedItems(first: 6, types: REPOSITORY) {\n nodes {\n ... on Repository {\n name\n }\n }\n }\n }\n}"' | jq --raw-output '.data.user.pinnedItems' | yq eval -P | sed "s/name: //g" > nodes.yaml
 
   for i in 0 1 2 3 4 5
   do
-	jq -r ".[$i]" nodes.json
     j=$(($i+1)) && NAME=$(yq eval ".nodes[$i]" nodes.yaml)
-    [ -z "${GITHUB_REPOSITORY##*$NAME*}" ] && TARGET=$(yq eval ".nodes[$j]" nodes.yaml)
+    [ -z "${GITHUB_REPOSITORY##*github.io*}" ] && REPOSITORY=${OWNER}/$(yq eval ".nodes[0]" nodes.yaml)
+    [[ -z "${GITHUB_REPOSITORY##*$NAME*}" && i < 5 ]] && REPOSITORY=${OWNER}/$(yq eval ".nodes[$j]" nodes.yaml)
   done
 
-  REPOSITORY=${GITHUB_REPOSITORY_OWNER}/${TARGET}
   rm -rf  nodes.* && rm -Rf -- */ && mv /maps/text/_* .
   [ -z "${REPOSITORY##*github.io*}" ] && mv /maps/_assets assets
 
-  JEKYLL_CFG=${GITHUB_WORKSPACE}/_config.yml
   mv /maps/_config.yml ${JEKYLL_CFG}
-
   sed -i "1s|^|target_repository: $REPOSITORY\n|" ${JEKYLL_CFG}
   sed -i "1s|^|repository: $GITHUB_REPOSITORY\n|" ${JEKYLL_CFG} && cat ${JEKYLL_CFG}
 
@@ -38,4 +32,11 @@ jekyll_build() {
   echo -e "\n$hr\nDEPLOY\n$hr" && deploy_remote "${REPOSITORY}"
 }
 
+set_owner() {
+  echo ${INPUT_TOKEN} | gh auth login --with-token
+  gh api -H "Accept: application/vnd.github+json" /user/orgs --jq "map(.login)" > nodes.json
+  OWNER=$(jq -r ".[1]" nodes.json)
+}
+
+[ -z "${GITHUB_REPOSITORY##*github.io*}" ] && set_owner
 echo -e "\n$hr\nJEKYLL BUILD\n$hr" && jekyll_build
