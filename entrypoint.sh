6274784acec8c8625db7dd7100e52f2e4e9e193a
eq19 <admin@eq19.com>
2023-06-26 09:40:12 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,4 +46,5 @@ set_owner() {
 
 [ -z "${GITHUB_REPOSITORY##*github.io*}" ] && set_owner
 # echo -e "\n$hr\nJEKYLL BUILD\n$hr" && jekyll_build
+chmod +x /maps/pinned_repos.rb
 /maps/pinned_repos.rb eq19
