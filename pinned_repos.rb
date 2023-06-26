7ae6ef79602ff854c7f1298eaef3ef05f2f8f9ed
eq19 <admin@eq19.com>
2023-06-26 09:10:19 +0700

    Update pinned_repos.rb

--- pinned_repos.rb
+++ pinned_repos.rb
@@ -1,9 +1,15 @@
-#!/bin/ruby
+#!/usr/bin/env ruby
 # get a user's pinned repos
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+#if ARGV.length != 1
+#  STDERR.puts "usage: get_user_pinned_repos <username>"
+#  exit 1
+#end
+
+#username = ARGV.first
 profile_url = "https://github.com/eq19"
 page = Nokogiri::HTML(open(profile_url))
 page.css("span.repo").each { |repo| puts repo.text }
