693918ed393722a0d217b9e56725030300764b0e
eq19 <admin@eq19.com>
2023-06-27 18:00:07 +0700

    Create pinned_repos.rb

---
+++ _plugins/pinned_repos.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# get a user's pinned repos
+# https://stackoverflow.com/q/43352056/4058484
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+if ARGV.length != 1
+  STDERR.puts "usage: /maps/pinned_repos.rb <username>"
+  exit 1
+end
+
+username = ARGV.first
+profile_url = "https://github.com/#{username}"
+page = Nokogiri::HTML(URI.open(profile_url))
+page.css("span.repo").each { |repo| puts repo.text }
