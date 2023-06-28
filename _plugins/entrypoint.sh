fa41194bc9e58dec3ae0582fe28c83848fbb0bd1
eq19 <admin@eq19.com>
2023-06-28 10:49:39 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -32,7 +32,7 @@ set_target() {
   elif [[ ${array[-1]} == $1 ]]; then TARGET_REPOSITORY=${OWNER}/${OWNER}.github.io
   else
     for i in 0 1 2 3 4 5; do
-      [[ ${array[$i]} == $1 && $i < 5 ]] && TARGET_REPOSITORY=${OWNER}/${array[$i+1]}
+      [[ "${array[$i]}" == "$1" && "$i" -lt 5 ]] && TARGET_REPOSITORY=${OWNER}/${array[$i+1]}
     done
   fi
 }
