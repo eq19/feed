57c35e48b72a7cbcac2fa517eb7e246ba0f76bff
eq19 <admin@eq19.com>
2023-06-28 12:15:04 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -30,11 +30,11 @@ set_target() {
   
   # Iterate the pinned repos
   printf -v array_str -- ',,%q' "${array[@]}"
-  if [[ ! "${array_str},," =~ ",,$1,," ]]; then TARGET_REPOSITORY=${OWNER}/${array[0]}
-  elif [[ "${array[-1]}" == "$1" ]]; then TARGET_REPOSITORY=${OWNER}/${OWNER}.github.io
+  if [[ ! "${array_str},," =~ ",,$1,," ]]; then TARGET_REPOSITORY=${array[0]}
+  elif [[ "${array[-1]}" == "$1" ]]; then TARGET_REPOSITORY=${OWNER}.github.io
   else
     for i in 0 1 2 3 4 5; do
-      [[ "${array[$i]}" == "$1" && "$i" -lt 5 ]] && TARGET_REPOSITORY=${OWNER}/${array[$i+1]}
+      [[ "${array[$i]}" == "$1" && "$i" -lt 5 ]] && TARGET_REPOSITORY=${array[$i+1]}
     done
   fi
 }
@@ -59,5 +59,5 @@ set_owner() {
 
 
 [[ ${GITHUB_REPOSITORY} != *"github.io"* ]]  && OWNER=${GITHUB_REPOSITORY_OWNER} || set_owner ${GITHUB_REPOSITORY_OWNER}
-set_target $(basename ${GITHUB_REPOSITORY})
-jekyll_build ${TARGET_REPOSITORY}
+set_target $(basename ${GITHUB_REPOSITORY}) 
+jekyll_build ${OWNER}/${TARGET_REPOSITORY}
