a50422038444b759de404107ddf9c954a42c8cd7
eq19 <admin@eq19.com>
2023-06-30 15:02:17 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -45,10 +45,10 @@ set_target() {
   fi
   
   # Generate id from the Structure
-  return ${ID}
+  return [[ -n "$3" ]] && $(( $ID + $3 * 6 )) || ${ID}
 }
 
 # https://unix.stackexchange.com/a/615292/158462
 [[ ${GITHUB_REPOSITORY} == *"github.io"* ]] && OWNER=$(set_target ${OWNER} ${GITHUB_ACTOR})
-TARGET_REPOSITORY=$(set_target $(basename ${GITHUB_REPOSITORY}) ${OWNER}.github.io)
+TARGET_REPOSITORY=$(set_target $(basename ${GITHUB_REPOSITORY}) ${OWNER}.github.io $?)
 jekyll_build ${OWNER}/${TARGET_REPOSITORY} $?
