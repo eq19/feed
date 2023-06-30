070181285358d921870a17ddf9700795279670ae
eq19 <admin@eq19.com>
2023-06-30 22:57:39 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -5,8 +5,8 @@
 jekyll_build() {
   
   echo -e "\n$hr\nCONFIG\n$hr"
-  rm -Rf -- */ && mv /maps/text/_* . && mv /maps/_config.yml ${JEKYLL_CFG}
-  if [[ $1 == *"github.io"* ]]; then mv /maps/_assets assets; fi
+  rm -Rf -- */ && cp /maps/text/_* . && cp /maps/_* .
+  if [[ $1 == *"github.io"* ]]; then mv _assets assets; fi
 
   sed -i "1s|^|target_repository: $1\n|" ${JEKYLL_CFG}
   sed -i "1s|^|repository: $GITHUB_REPOSITORY\n|" ${JEKYLL_CFG}
