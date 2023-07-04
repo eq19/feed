fe4628500fb76340e677c12a0bbfb691d909c26c
eq19 <admin@eq19.com>
2023-07-04 23:21:56 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -6,7 +6,7 @@ set_target() {
   
   # Get Structure
   if [[ "$2" == *"github.io"* ]]; then
-    [[ -n "$3" ]] && SPIN=`expr $3 \* 6`
+    [[ -n "$ID" ]] && SPIN=`expr $3 \* 6`
     IFS=', '; array=($(pinned_repos.rb ${OWNER} | yq eval -P | sed "s/ /, /g"))
   else
     HEADER="Accept: application/vnd.github+json"
