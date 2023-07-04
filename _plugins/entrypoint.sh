d010c083fa4cc16dbf73d394150faa89ed4c8376
eq19 <admin@eq19.com>
2023-07-04 22:29:59 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -61,5 +61,6 @@ jekyll_build() {
 
 # https://unix.stackexchange.com/a/615292/158462
 [[ ${OWNER} != ${GITHUB_ACTOR} || ${GITHUB_REPOSITORY} == *"github.io"* ]] && OWNER=$(set_target ${OWNER} ${GITHUB_ACTOR})
+echo test $?
 TARGET_REPOSITORY=$(set_target $(basename ${GITHUB_REPOSITORY}) ${OWNER}.github.io $?)
 jekyll_build ${OWNER}/${TARGET_REPOSITORY} $?
