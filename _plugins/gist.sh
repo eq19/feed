c75596531271bd5b52fc492f6260fc32e5b7c286
eq19 <admin@eq19.com>
2023-07-16 21:26:39 +0700

    Create gist.sh

---
+++ _plugins/gist.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+clone_gist() {
+
+  PATTERN="sort_by(.created_at)|.[] | select(.public==true).files.[].raw_url"
+  gh api -H "${HEADER}" /users/eq19/gists --jq "${PATTERN}" > /tmp/gist_files
+  gh gist clone 0ce5848f7ad62dc46dedfaa430069857 /maps/_includes/workdir/addition &>/dev/null
+  gh gist clone 5b26b3cd8dc42d94ef240496ad56a54f /maps/_includes/workdir/multiplication &>/dev/null
+
+}
