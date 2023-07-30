24b5d9323fd1189fc0ae0d18b308ff239762fcc7
eq19 <admin@eq19.com>
2023-07-31 06:39:05 +0700

    Update gist.sh

--- _plugins/gist.sh
+++ _plugins/gist.sh
@@ -11,12 +11,12 @@ mv -f /tmp/workdir/file02.md /tmp/workdir/multiplication/README.md
 
 gh gist clone bbfac33187aae966e2faedf253f7b703 /tmp/workdir/exponentiation
 mv -f /tmp/workdir/file03.md /tmp/workdir/exponentiation/README.md
-gh gist clone e9832026b5b78f694e4ad22c3eb6c3ef /tmp/workdir/exponentiation/folder18
-gh gist clone e84a0961dc7636c01d5953d19d65e30a /tmp/workdir/exponentiation/folder17
-gh gist clone dc30497160f3389546d177da901537d9 /tmp/workdir/exponentiation/folder16
-gh gist clone b9f901cda16e8a11dd24ee6b677ca288 /tmp/workdir/exponentiation/folder15
-gh gist clone 765ddc69e339079a5a64b56c1d46e00f /tmp/workdir/exponentiation/folder14
 gh gist clone f78d4470250720fb18111165564d555f /tmp/workdir/exponentiation/folder13
+gh gist clone 765ddc69e339079a5a64b56c1d46e00f /tmp/workdir/exponentiation/folder14
+gh gist clone b9f901cda16e8a11dd24ee6b677ca288 /tmp/workdir/exponentiation/folder15
+gh gist clone dc30497160f3389546d177da901537d9 /tmp/workdir/exponentiation/folder16
+gh gist clone e84a0961dc7636c01d5953d19d65e30a /tmp/workdir/exponentiation/folder17
+gh gist clone e9832026b5b78f694e4ad22c3eb6c3ef /tmp/workdir/exponentiation/folder18
 
 gh gist clone fd5135db1c295af4d3dae5f95f6891ea /tmp/workdir/identition
 mv -f /tmp/workdir/file04.md /tmp/workdir/identition/README.md
@@ -31,8 +31,5 @@ gh gist clone f21abd90f8d471390aad23d6ecc90d6d /tmp/workdir/identition/folder8
 gh gist clone 6e2fcc2138be6fb68839a3ede32f0525 /tmp/workdir/identition/folder9
 gh gist clone b541275ab7deda356feef32d600e44d8 /tmp/workdir/identition/folder10
 gh gist clone 80c8098f16f3e6ca06893b17a02d910e /tmp/workdir/identition/folder11
-
-gh gist clone 5b26b3cd8dc42d94ef240496ad56a54f /tmp/workdir/identition/folder12
-mv -f /tmp/workdir/file05.md /tmp/workdir/identition/folder12/README.md
 
 find /tmp/workdir -type d -name .git -prune -exec rm -rf {} \;
