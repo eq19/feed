7ef61ac7c0e8992f554de5c3bb46a2d1a2366abb
eq19 <admin@eq19.com>
2023-07-30 09:47:01 +0700

    file0

--- _plugins/gist.sh
+++ _plugins/gist.sh
@@ -4,13 +4,13 @@ rm -rf /tmp/workdir
 
 gh gist clone c9bdc2bbe55f2d162535023c8d321831 /tmp/workdir
 gh gist clone 0ce5848f7ad62dc46dedfaa430069857 /tmp/workdir/addition
-mv -f /tmp/workdir/file1.md /tmp/workdir/addition/README.md
+mv -f /tmp/workdir/file01.md /tmp/workdir/addition/README.md
 
 gh gist clone 4ffc4d02579d5cfd336a553c6da2f267 /tmp/workdir/multiplication
-mv -f /tmp/workdir/file2.md /tmp/workdir/multiplication/README.md
+mv -f /tmp/workdir/file02.md /tmp/workdir/multiplication/README.md
 
 gh gist clone bbfac33187aae966e2faedf253f7b703 /tmp/workdir/exponentiation
-mv -f /tmp/workdir/file3.md /tmp/workdir/exponentiation/README.md
+mv -f /tmp/workdir/file03.md /tmp/workdir/exponentiation/README.md
 gh gist clone e9832026b5b78f694e4ad22c3eb6c3ef /tmp/workdir/exponentiation/folder18
 gh gist clone e84a0961dc7636c01d5953d19d65e30a /tmp/workdir/exponentiation/folder17
 gh gist clone dc30497160f3389546d177da901537d9 /tmp/workdir/exponentiation/folder16
@@ -19,7 +19,7 @@ gh gist clone 765ddc69e339079a5a64b56c1d46e00f /tmp/workdir/exponentiation/folde
 gh gist clone f78d4470250720fb18111165564d555f /tmp/workdir/exponentiation/folder13
 
 gh gist clone fd5135db1c295af4d3dae5f95f6891ea /tmp/workdir/identition
-mv -f /tmp/workdir/file4.md /tmp/workdir/identition/README.md
+mv -f /tmp/workdir/file04.md /tmp/workdir/identition/README.md
 gh gist clone 0ce5848f7ad62dc46dedfaa430069857 /tmp/workdir/identition/folder1
 gh gist clone b32915925d9d365e2e9351f0c4ed786e /tmp/workdir/identition/folder2
 gh gist clone 88d09204b2e5986237bd66d062406fde /tmp/workdir/identition/folder3
@@ -33,6 +33,6 @@ gh gist clone b541275ab7deda356feef32d600e44d8 /tmp/workdir/identition/folder10
 gh gist clone 80c8098f16f3e6ca06893b17a02d910e /tmp/workdir/identition/folder11
 
 gh gist clone 5b26b3cd8dc42d94ef240496ad56a54f /tmp/workdir/identition/folder12
-mv -f /tmp/workdir/file5.md /tmp/workdir/identition/folder12/README.md
+mv -f /tmp/workdir/file05.md /tmp/workdir/identition/folder12/README.md
 
 find /tmp/workdir -type d -name .git -prune -exec rm -rf {} \;
