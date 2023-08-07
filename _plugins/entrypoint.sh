fa48132830d91c79fbd76dbccf53747dbea8eef8
Chetabahana <chetabahana@gmail.com>
2023-08-07 20:50:35 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -7,7 +7,7 @@ set_target() {
   # Get Structure
   if [[ $2 == *"github.io"* ]]; then
     [[ -n "$CELL" ]] && SPIN=$(( CELL * 7 ))
-    pinned_repos.rb ${OWNER} | yq eval -P | sed "s/ /, /g" > /tmp/pinned_repo
+    pinned_repos.rb ${ENTRY} | yq eval -P | sed "s/ /, /g" > /tmp/pinned_repo
     IFS=', '; array=($(cat /tmp/pinned_repo))
   else
     gh api -H "${HEADER}" /user/orgs --jq '.[].description' | sort -uf | yq eval -P | sed "s/ /, /g" > /tmp/desc
