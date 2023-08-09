858f71619b245e1d294c09cbcbdd7f20cfe5f7ec
eq19 <admin@eq19.com>
2023-08-10 06:17:34 +0700

    Update hello-world.rb

--- _plugins/gems/hello-world.rb
+++ _plugins/gems/hello-world.rb
@@ -7,7 +7,7 @@ module Jekyll
     end
 
     def render(context)
-      "This section is #{@text} by [the spin]({{ item_baseline | prepend:"https://" | append: ".github.io/" }})"
+      "This section is #{@text}"
     end
   end
 end
