ca2edb8b59dfa9bc3329a04da1b0b2bb5888f3c9
Chetabahana <chetabahana@gmail.com>
2023-08-15 19:03:30 +0700

    Update gist.sh

--- _plugins/gist.sh
+++ _plugins/gist.sh
@@ -23,7 +23,7 @@ gh gist clone 80c8098f16f3e6ca06893b17a02d910e /tmp/gistdir/identition/folder10
 gh gist clone 4ffc4d02579d5cfd336a553c6da2f267 /tmp/gistdir/identition/folder11
 
 find /tmp/gistdir -type f -name "README.md" -prune -exec rm -rf {} \;
-find /tmp/workdir/identition -type f -name "*.md" -prune -exec sh -c 'mv -f "$1" README.md' sh {} \;
+find /tmp/workdir/identition -type f -name "*.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
 
 gh gist clone f78d4470250720fb18111165564d555f /tmp/gistdir/exponentiation/folder13
 gh gist clone 765ddc69e339079a5a64b56c1d46e00f /tmp/gistdir/exponentiation/folder14
