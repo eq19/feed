b4b63cb3ef26e6456a2842c6fe5079418517bcbd
eq19 <admin@eq19.com>
2023-08-24 22:28:44 +0700

    Update gist.sh

--- _plugins/gist.sh
+++ _plugins/gist.sh
@@ -28,8 +28,8 @@ gh gist clone dc30497160f3389546d177da901537d9 /tmp/gistdir/exponentiation/folde
 gh gist clone e84a0961dc7636c01d5953d19d65e30a /tmp/gistdir/exponentiation/folder17
 gh gist clone e9832026b5b78f694e4ad22c3eb6c3ef /tmp/gistdir/exponentiation/folder18
 
+find /tmp/gistdir -type d -name .git -prune -exec rm -rf {} \;
 find /tmp/workdir -type f -name "Home.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
 find /tmp/workdir/identition -type f -name "*.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
 find /tmp/workdir/exponentiation -type f -name "*.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
 find /tmp/gistdir -type f -name "*zone.md" -prune -exec sh -c 'mkdir -p "${1%_*}" && mv -f "$1" "${1%_*}/README.md"' sh {} \;
-find /tmp/gistdir -type d -name .git -prune -exec rm -rf {} \; && find /tmp/gistdir -type f -name "README.md" -exec rm -rf {} \;
