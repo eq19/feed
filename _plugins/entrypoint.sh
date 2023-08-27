78392ffe1fe09823592c2d970e7305276900d15d
Chetabahana <chetabahana@gmail.com>
2023-08-27 15:59:18 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -15,8 +15,8 @@ set_target() {
 
     echo "[" > /tmp/orgs.json
     for ((i=0; i < ${#array[@]}; i++)); do
-      PIN2=$(pinned_repos.rb ${array[$i]} | yq eval -P | sed "s/ /| /g")
-      PIN2="maps|feed|lexer|parser|syntax|grammar"
+      PIN2=$(pinned_repos.rb ${array[$i]} | yq eval -P | sed "s/ /; /g")
+      PIN2="maps;feed;lexer;parser;syntax;grammar"
       gh api -H "${HEADER}" /orgs/${array[$i]} | jq '. + {"key1": "value1"} + {"key2": "'$PIN2'"}' >> /tmp/orgs.json
       if [[ "$i" -lt "${#array[@]}-1" ]]; then echo "," >> /tmp/orgs.json; fi
     done
