f536c4b430fd01c890553f3112c214aef6d31a21
Chetabahana <chetabahana@gmail.com>
2023-08-27 19:56:30 +0700

    Update pinned_repos.rb

--- _plugins/pinned_repos.rb
+++ _plugins/pinned_repos.rb
@@ -7,10 +7,10 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-if ARGV.length != 1
-  STDERR.puts "usage: /maps/pinned_repos.rb <username>"
-  exit 1
-end
+# if ARGV.length != 1
+#  STDERR.puts "usage: /maps/pinned_repos.rb <username>"
+#  exit 1
+# end
 
 username = ARGV.first
 profile_url = "https://github.com/#{username}"
