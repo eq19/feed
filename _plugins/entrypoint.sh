0fd608b2b7ffd829ef783e739987fa11f35ee114
Chetabahana <chetabahana@gmail.com>
2023-08-27 11:37:29 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -15,7 +15,16 @@ set_target() {
 
     echo "[" > /tmp/orgs.json
     for ((i=0; i < ${#array[@]}; i++)); do
-      MYPATH='{"bucketname":"%s","objectname":"%s","targetlocation":"%s"}\n'
+    
+BUCKET_NAME=testbucket
+OBJECT_NAME=testworkflow-2.0.1.jar
+TARGET_LOCATION=/opt/test/testworkflow-2.0.1.jar
+
+MYPATH=$( jq -n \
+                  --arg bn "$BUCKET_NAME" \
+                  --arg on "$OBJECT_NAME" \
+                  --arg tl "$TARGET_LOCATION" \
+                  '{bucketname: $bn, objectname: $on, targetlocation: $tl}' )
       gh api -H "${HEADER}" /orgs/${array[$i]} | jq '. + {"key1": "value1"} + {"key2": $MYPATH}' >> /tmp/orgs.json
       if [[ "$i" -lt "${#array[@]}-1" ]]; then echo "," >> /tmp/orgs.json; fi
     done
