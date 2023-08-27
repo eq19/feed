d176c0f4e0d8b690d2cb24be69b65ed7042a334a
Chetabahana <chetabahana@gmail.com>
2023-08-27 12:23:42 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -16,7 +16,7 @@ set_target() {
     echo "[" > /tmp/orgs.json
     for ((i=0; i < ${#array[@]}; i++)); do
       MYPATH="value2"
-      gh api -H "${HEADER}" /orgs/${array[$i]} | jq '. + {"key1": "value1"} + {"key2": $MYPATH}' >> /tmp/orgs.json
+      gh api -H "${HEADER}" /orgs/${array[$i]} | jq '. + {"key1": "value1"} + {"key2": "'$MYPATH'"}' >> /tmp/orgs.json
       if [[ "$i" -lt "${#array[@]}-1" ]]; then echo "," >> /tmp/orgs.json; fi
     done
     echo "]" >> /tmp/orgs.json
