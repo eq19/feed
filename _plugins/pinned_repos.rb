00b94bcc961ebe24d17432f21573a5c76b7683a7
Chetabahana <chetabahana@gmail.com>
2023-08-28 09:14:02 +0700

    Update pinned_repos.rb

--- _plugins/pinned_repos.rb
+++ _plugins/pinned_repos.rb
@@ -2,7 +2,9 @@
 
 # get a user's pinned repos
 # https://stackoverflow.com/q/43352056/4058484
+# https://stackoverflow.com/q/7785793/4058484
 
+require 'uri'
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
@@ -16,14 +18,14 @@ token = ENV['INPUT_TOKEN']
 username = ARGV[0]
 mode = ARGV[1]
 
-# profile_url = "https://github.com/#{username}?view_as=#{mode}"
 # open("http://...", :http_basic_authentication=>[user, password])
+# profile_url = "https://github.com/#{username}"
 
-profile_url = "https://github.com/#{username}"
+profile_url = "https://github.com/#{username}?view_as=#{mode}"
+
 uri = URI.parse( profile_url )
-
 params = { :view_as => "#{mode}" }
 uri.query = URI.encode_www_form( params )
 
-page = Nokogiri::HTML(URI.open(uri, "Authorization" => "Bearer #{token}"))
+page = Nokogiri::HTML(URI.open(profile_url, "Authorization" => "Bearer #{token}"))
 page.css("span.repo").each { |repo| puts repo.text }
