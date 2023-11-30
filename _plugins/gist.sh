a4756390e4f379cb696ac38511f101cea5f45173
₠Quantum <alzheimermom@gmail.com>
2023-12-01 05:05:47 +0700

    Update gist.sh

--- _plugins/gist.sh
+++ _plugins/gist.sh
@@ -10,7 +10,7 @@ if [[ $? == 0 ]]; then git clone $WIKI /tmp/workdir;
 else git clone $BASE /tmp/workdir && rm -rf /tmp/workdir/Home.md; fi
 
 mv -f /tmp/workdir/virtual /maps/_includes/virtual && mkdir /tmp/gistdir
-cp -R /maps/_drafts/* /tmp/gistdir
+#cp -R /maps/_drafts/* /tmp/gistdir
 
 gh gist clone 0ce5848f7ad62dc46dedfaa430069857 /tmp/gistdir/identition/span1
 gh gist clone b32915925d9d365e2e9351f0c4ed786e /tmp/gistdir/identition/span2
@@ -33,11 +33,9 @@ gh gist clone e84a0961dc7636c01d5953d19d65e30a /tmp/gistdir/exponentiation/span1
 gh gist clone e9832026b5b78f694e4ad22c3eb6c3ef /tmp/gistdir/exponentiation/span18
 
 find /tmp/workdir -type f -name "Home.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
+find /tmp/workdir -type f -name "file01.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
+find /tmp/workdir -type f -name "file02.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
 find /tmp/workdir/identition -type f -name "*.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
 find /tmp/workdir/exponentiation -type f -name "*.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
 find /tmp/gistdir -type d -name .git -prune -exec rm -rf {} \; && find /tmp/gistdir -type f -name "README.md" -exec rm -rf {} \;
 find /tmp/gistdir -type f -name "*zone.md" -prune -exec sh -c 'mkdir --parent "${1%_*}" && mv -f "$1" "${1%_*}/README.md"' sh {} \;
-#find /tmp/gistdir -type f -name "file01.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
-#find /tmp/gistdir -type f -name "file02.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
-#find /tmp/gistdir -type f -name "file03.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
-#find /tmp/gistdir -type f -name "file04.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
