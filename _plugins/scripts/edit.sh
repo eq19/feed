6d83fe33a09f438e624f0077e7cc2a8b15ac3f90
₠Quantum <alzheimermom@gmail.com>
2023-12-05 05:06:20 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-IFS=$'\n' read -d '' -r -a array < _Sidebar.md
-
 echo $1
+sed -i 's/💎:/sort:/g' $1
+sed -i 's/🚀:/spin:/g' $1
+sed -i 's/🔨:/span:/g' $1
+sed -i 's/📂:/suit:/g' $1
+
+IFS=$'\n' read -d '' -r -a array < _Sidebar.md
 echo ${array[2]};
