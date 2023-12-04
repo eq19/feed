98a347298b537ac0e8cafca334974d1f2543a551
₠Quantum <alzheimermom@gmail.com>
2023-12-04 20:11:04 +0700

    Update entrypoint.sh

--- _plugins/entrypoint.sh
+++ _plugins/entrypoint.sh
@@ -71,8 +71,8 @@ jekyll_build() {
   find . -type f -name "*.md" -exec sed -i 's/🔨:/span:/g' {} +
   find . -type f -name "*.md" -exec sed -i 's/📂:/suit:/g' {} +
   
-  find . -type f -name "*.md" -exec echo {} \;
-  #find . -type f -name "*.md" -exec /maps/_plugins/edit.sh {} \;
+  #find . -type f -name "*.md" -exec echo {} \;
+  find . -type f -name "*.md" -exec /maps/_plugins/edit.sh {} \;
   # Jekyll Quick Reference https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
   JEKYLL_GITHUB_TOKEN=${INPUT_TOKEN} bundle exec jekyll build --profile -t -p /maps/_plugins/gems
   
