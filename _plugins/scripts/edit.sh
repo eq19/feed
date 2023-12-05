55bb97a9c5f347780b453227c14d48729e976a0e
₠Quantum <alzheimermom@gmail.com>
2023-12-05 20:54:34 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-FILE=${1##*/} && SORT=${FILE%.*}
+FILE=${1##*/}
+SORT=${FILE%.*}
 
 sed -i 's/💎:/sort:/g' $1
 sed -i 's/🚀:/spin:/g' $1
 sed -i 's/🔨:/span:/g' $1
 sed -i 's/📂:/suit:/g' $1
-
-IFS=$'\n' read -d '' -r -a LINE < _Sidebar.md
-TEXT=${LINE[$SORT]} && TITLE=${TEXT%|*}
 
 while IFS=',' read -ra SPIN; do
   S+=("${SPIN[0]}")
@@ -16,6 +14,9 @@ while IFS=',' read -ra SPIN; do
   I+=("${SPIN[2]}")
   N+=("${SPIN[3]}")
 done < exponentiation/span18/spin.csv
+
+IFS=$'\n' read -d '' -r -a LINE < _Sidebar.md
+TEXT=${LINE[$SORT]} && TITLE=${TEXT%|*}
 
 FRONT="---\n"
 FRONT+="sort: $SORT\n"
