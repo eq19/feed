e93cd73ea0ab09b626fc90656531703f9dddb95c
₠Quantum <alzheimermom@gmail.com>
2023-12-05 19:08:31 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -8,11 +8,13 @@ sed -i 's/🔨:/span:/g' $1
 sed -i 's/📂:/suit:/g' $1
 
 IFS=$'\n' read -d '' -r -a LINE < _Sidebar.md
+IFS=, read -r s p i n < exponentiation/span18/spin.csv
+  
 TEXT=${LINE[$SORT]} && TITLE=${TEXT%|*}
 
 FRONT="---\n"
 FRONT+="sort: $SORT\n"
-FRONT+="suit: 2\n"
+FRONT+="suit: $s[$SORT]\n"
 FRONT+="---\n"
 FRONT+="# $TITLE\n"
 FRONT+="{% include list.liquid all=true %}\n\n"
