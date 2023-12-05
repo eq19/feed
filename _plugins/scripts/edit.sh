1414db5ffefb3c28af754ad1d759e1f204d1c441
₠Quantum <alzheimermom@gmail.com>
2023-12-05 13:20:10 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -8,10 +8,9 @@ sed -i 's/🔨:/span:/g' $1
 sed -i 's/📂:/suit:/g' $1
 
 PATH=${1##*/} && SORT=${PATH%.*}
-IFS=$'\n' read -d '' -r -a array < _Sidebar.md && echo ${array[2]};
+IFS=$'\n' read -d '' -r -a array < _Sidebar.md
+echo ${array[2]};
 
 if [[ "$SORT" == "2" ]]; then
   sed -i "1s|^|---\nsort: $SORT\n---\n|" $1
 fi
-
-
