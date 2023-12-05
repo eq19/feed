3d92fe710161cf74b2a1d802290ead60b2e359b0
₠Quantum <alzheimermom@gmail.com>
2023-12-05 12:43:46 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -8,7 +8,7 @@ sed -i 's/🔨:/span:/g' $1
 sed -i 's/📂:/suit:/g' $1
 
 PATH=${1##*/} && NUM=${PATH%.*}
-[[ $NUM == "2" ]] && echo match
+[[ "$NUM" == "2" ]] && echo match
 #sed -i "1s|^|---\nsort: $n\n---\n|" $1
 
 IFS=$'\n' read -d '' -r -a array < _Sidebar.md
