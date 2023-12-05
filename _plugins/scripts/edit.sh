b1b24fbd4aa654ca40c7fafdced89cd3f6970af3
₠Quantum <alzheimermom@gmail.com>
2023-12-05 12:44:59 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -7,9 +7,9 @@ sed -i 's/🚀:/spin:/g' $1
 sed -i 's/🔨:/span:/g' $1
 sed -i 's/📂:/suit:/g' $1
 
-PATH=${1##*/} && NUM=${PATH%.*}
-[[ "$NUM" == "2" ]] && echo match
-#sed -i "1s|^|---\nsort: $n\n---\n|" $1
+PATH=${1##*/} && SORT=${PATH%.*}
+[[ "$SORT" == "2" ]] && echo match
+#sed -i "1s|^|---\nsort: $SORT\n---\n|" $1
 
 IFS=$'\n' read -d '' -r -a array < _Sidebar.md
 echo ${array[2]};
