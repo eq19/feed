506d0fda47a8417e089a9e686b24cc7c2461d680
₠Quantum <alzheimermom@gmail.com>
2023-12-05 12:29:35 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 echo $1
-s=${1##*/} && echo "${{1##*/}%.*}"
+
 sed -i 's/💎:/sort:/g' $1
 sed -i 's/🚀:/spin:/g' $1
 sed -i 's/🔨:/span:/g' $1
 sed -i 's/📂:/suit:/g' $1
+
+s=${1##*/} && n = "${s%.*}"
+[[ $n == "2" ]] && echo match
+#sed -i "1s|^|sort: $n\n|" $1
 
 IFS=$'\n' read -d '' -r -a array < _Sidebar.md
 echo ${array[2]};
