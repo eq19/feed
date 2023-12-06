6b5b0d4b5bf76306d0e803211694a3ffc6aa70d2
₠Quantum <alzheimermom@gmail.com>
2023-12-06 16:03:42 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -18,7 +18,7 @@ while IFS=' ' read -ra SPIN; do
   P+=("${SPIN[1]}")
   I+=("${SPIN[2]}")
   N+=("${SPIN[3]}")
-done < $DATA
+done < exponentiation/span18/spin1.txt
 
 IFS=$'\n' read -d '' -r -a LINE < _Sidebar.md
 TEXT=${LINE[$SORT]} && TITLE=${TEXT%|*}
