28b37ea11fa4955f5288805796026bfb707c478c
₠Quantum <alzheimermom@gmail.com>
2023-12-06 20:59:47 +0700

    Update edit.sh

--- _plugins/scripts/edit.sh
+++ _plugins/scripts/edit.sh
@@ -1,39 +1,40 @@
 #!/usr/bin/env bash
 
-LINE=$1
-FILE=${2##*/}
-SORT=${FILE%.*}
-NUM=$(($SORT + 0))
-TEXT="${LINE[$SORT]}" && TITLE=${TEXT%|*}
+sed -i 's/💎:/sort:/g' $1
+sed -i 's/🚀:/spin:/g' $1
+sed -i 's/🔨:/span:/g' $1
+sed -i 's/📂:/suit:/g' $1
 
-sed -i 's/💎:/sort:/g' $2
-sed -i 's/🚀:/spin:/g' $2
-sed -i 's/🔨:/span:/g' $2
-sed -i 's/📂:/suit:/g' $2
+edit_file () {
+  NUM=$(($2 + 0))
+  
+  IFS=$'\n' read -d '' -r -a LINE < _Sidebar.md
+  TEXT="${LINE[$NUM]}" && TITLE=${TEXT%|*}
 
-cat exponentiation/span18/spin1.txt > /tmp/spin.txt
-
-if [[ $SORT =~ ^-?[0-9]+$ ]]; then
+  cat exponentiation/span18/spin1.txt > /tmp/spin.txt
   [[ $NUM -ge 10 ]] && cat exponentiation/span18/spin2.txt >> /tmp/spin.txt
   [[ $NUM -ge 22 ]] && cat exponentiation/span18/spin3.txt >> /tmp/spin.txt
   [[ $NUM -ge 28 ]] && cat exponentiation/span18/spin4.txt >> /tmp/spin.txt
-fi
 
-while IFS=' ' read -ra SPIN; do
-  S+=("${SPIN[0]}")
-  P+=("${SPIN[1]}")
-  I+=("${SPIN[2]}")
-  N+=("${SPIN[3]}")
-done < /tmp/spin.txt
+  while IFS=' ' read -ra SPIN; do
+    S+=("${SPIN[0]}")
+    P+=("${SPIN[1]}")
+    I+=("${SPIN[2]}")
+    N+=("${SPIN[3]}")
+  done < /tmp/spin.txt
 
-FRONT="---\n"
-FRONT+="sort: $SORT\n"
-FRONT+="suit: ${S[$SORT]}\n"
-FRONT+="---\n"
-FRONT+="# $TITLE\n\n"
+  FRONT="---\n"
+  FRONT+="sort: $2\n"
+  FRONT+="suit: ${S[$2]}\n"
+  FRONT+="---\n"
+  FRONT+="# $TITLE\n\n"
 
-[[ $SORT =~ ^-?[0-9]+$ && $NUM -le 9 ]] && sed -i "1s|^|$FRONT|" $2
-if [[ "$SORT" == "0" || "$SORT" == "1" || "$SORT" == "9" ]]; then
-  mv -f $2 ${2%/*}/README.md
-  sed '1,6!d' ${2%/*}/README.md
-fi
+  [[ $NUM -le 9 ]] && sed -i "1s|^|$FRONT|" $1
+  if [[ "$SORT" == "0" || "$SORT" == "1" || "$SORT" == "9" ]]; then
+    mv -f $1 ${1%/*}/README.md
+    sed '1,6!d' ${1%/*}/README.md
+  fi
+}
+
+FILE=${1##*/} && SORT=${FILE%.*}
+[[ $SORT =~ ^-?[0-9]+$ ]] && edit_file $1 $SORT
