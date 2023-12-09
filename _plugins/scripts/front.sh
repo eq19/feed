c43797e0f3fb0ade8a397fcba33a1fb38ab8d3e0
₠Quantum <alzheimermom@gmail.com>
2023-12-09 16:17:54 +0700

    Update front.sh

--- _plugins/scripts/front.sh
+++ _plugins/scripts/front.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-sed -i 's/💎:/sort:/g' $1
-sed -i 's/🚀:/spin:/g' $1
-sed -i 's/🔨:/span:/g' $1
-sed -i 's/📂:/suit:/g' $1
-
 edit_file () {
+
   NUM=$(($2 + 0))
-  
-  IFS=$'\n' read -d '' -r -a LINE < _Sidebar.md
-  TEXT="${LINE[$NUM]}" && TITLE=${TEXT%|*}
 
   while IFS=' ' read -ra SPIN; do
     T+=("${SPIN[0]}")
@@ -26,6 +19,9 @@ edit_file () {
   FRONT+="sort: $NUM\n"
   FRONT+="suit: ${T[$NUM]}\n"
   FRONT+="---\n"
+  
+  IFS=$'\n' read -d '' -r -a LINE < _Sidebar.md
+  TEXT="${LINE[$NUM]}" && TITLE=${TEXT%|*}
   FRONT+="# $TITLE\n\n"
 
   [[ $NUM -le 9 ]] && sed -i "1s|^|$FRONT|" $1
@@ -34,6 +30,11 @@ edit_file () {
     sed '1,6!d' ${1%/*}/README.md
   fi
 }
+
+sed -i 's/💎:/sort:/g' $1
+sed -i 's/🚀:/spin:/g' $1
+sed -i 's/🔨:/span:/g' $1
+sed -i 's/📂:/suit:/g' $1
 
 FILE=${1##*/} && SORT=${FILE%.*}
 [[ $SORT =~ ^-?[0-9]+$ ]] && edit_file $1 $SORT
