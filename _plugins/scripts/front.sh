4799eaa6373f0206a90eeb789c5bc86fcdba7f3f
₠Quantum <alzheimermom@gmail.com>
2023-12-10 06:08:10 +0700

    Update front.sh

--- _plugins/scripts/front.sh
+++ _plugins/scripts/front.sh
@@ -17,7 +17,7 @@ edit_file () {
 
   FRONT="---\n"
   FRONT+="sort: $NUM\n"
-  FRONT+="suit: ${K[$NUM]}\n"
+  FRONT+="suit: ${I[$NUM]}\n"
   FRONT+="---\n"
   
   IFS=$'\n' read -d '' -r -a LINE < _Sidebar.md
