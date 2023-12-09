15d901f2ccc32394a4abe1ae9617410f241549f4
₠Quantum <alzheimermom@gmail.com>
2023-12-09 22:49:28 +0700

    Update sort.sh

--- _plugins/scripts/sort.sh
+++ _plugins/scripts/sort.sh
@@ -5,11 +5,13 @@ edit_file () {
   #NUM=$(($1 + 0))
 
   while IFS=' ' read -ra SORT; do
-    S+=("${SORT[0]}")
-    O+=("${SORT[1]}")
-    R+=("${SORT[2]}")
-    T+=("${SORT[3]}")
-    echo "${SORT[0]} ${SORT[1]} ${SORT[2]} ${SORT[3]} ${SORT[0]}" >> /tmp/spin.txt
+   
+    S+=("${SORT[0]}") && SPIN  ="${SORT[0]}"
+    O+=("${SORT[1]}") && SPIN +="${SORT[1]}"
+    R+=("${SORT[2]}") && SPIN +="${SORT[2]}"
+    T+=("${SORT[3]}") && SPIN +="${SORT[3]}"
+    echo "$SPIN ${SORT[0]}" >> /tmp/spin.txt
+
   done < $1
 
   cat /tmp/spin.txt
