cce96d2f8e853e31afee34409b520d1d5cd4483f
₠Quantum <alzheimermom@gmail.com>
2023-12-10 11:22:03 +0700

    Update sort.sh

--- _plugins/scripts/sort.sh
+++ _plugins/scripts/sort.sh
@@ -4,24 +4,24 @@ edit_file () {
 
   while ((i++)); IFS=' ' read -ra SORT; do
 
-    S+=("${SORT[0]}")
-    O+=("${SORT[1]}")
-    R+=("${SORT[2]}")
-    T+=("${SORT[3]}")
-    I+=("${SORT[1]}")
-    N+=("${SORT[2]}")
-    G+=("${SORT[3]}")
+    T+=("${SORT[0]}")
+    R+=("${SORT[1]}")
+    A+=("${SORT[2]}")
+    C+=("${SORT[3]}")
+    K+=("${SORT[4]}")
+    I+=("${SORT[5]}")
+    N+=("${SORT[6]}")
+    G+=("${SORT[7]}")
 
   done < /tmp/spin.txt
 
   while ((j++)); IFS=' ' read -ra SORT; do
 
     SPIN="$(( i + j ))"
-   
-    S+=("${SORT[0]}") && SPIN+=" ${SORT[0]}"
-    O+=("${SORT[1]}") && SPIN+=" ${SORT[1]}"
-    R+=("${SORT[2]}") && SPIN+=" ${SORT[2]}"
-    T+=("${SORT[3]}") && SPIN+=" ${SORT[3]}"
+    SPIN+=" ${SORT[0]}"
+    SPIN+=" ${SORT[1]}"
+    SPIN+=" ${SORT[2]}"
+    SPIN+=" ${SORT[3]}"
     
     echo "${SPIN} ${SORT[0]}" >> /tmp/spin.txt
 
