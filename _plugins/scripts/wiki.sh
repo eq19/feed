9c5d2374ee29b0ce336b53b28c80a26636ba6377
eQuantum <alzheimermom@gmail.com>
2024-05-02 00:05:55 +0700

    Update wiki.sh

--- _plugins/scripts/wiki.sh
+++ _plugins/scripts/wiki.sh
@@ -8,9 +8,7 @@ BASE=https://github.com/eq19/eq19.github.io.wiki.git
 
 #git ls-remote ${WIKI} > /dev/null 2>&1
 #if [[ $? == 0 ]]; then git clone $WIKI /tmp/workdir;
-#else git clone $BASE /tmp/workdir && rm -rf /tmp/workdir/Home.md; fi
-git clone $BASE /tmp/workdir
-mv -f /tmp/workdir/virtual /maps/_includes/virtual && mkdir /tmp/gistdir
+git clone $BASE /tmp/workdir && mkdir /tmp/gistdir
 
 gh gist clone 0ce5848f7ad62dc46dedfaa430069857 /tmp/gistdir/identition/span1
 gh gist clone b32915925d9d365e2e9351f0c4ed786e /tmp/gistdir/identition/span2
