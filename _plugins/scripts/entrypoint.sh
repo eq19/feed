d1b01dfd84b2f51935d2176ca0f34054cfb0f44a
eQuantum <alzheimermom@gmail.com>
2024-05-02 11:46:10 +0700

    Update entrypoint.sh

--- _plugins/scripts/entrypoint.sh
+++ _plugins/scripts/entrypoint.sh
@@ -76,7 +76,7 @@ jekyll_build() {
   cat /tmp/spin.txt
   #find . -iname '*.md' -print0 | sort -zn | xargs -0 -I '{}' echo '{}'
   find . -iname '*.md' -print0 | sort -zn | xargs -0 -I '{}' front.sh '{}' $1 $2 $3
-ls -R /tmp/workdir/exponentiation/span17
+
   # Jekyll Quick Reference https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
   JEKYLL_GITHUB_TOKEN=${INPUT_TOKEN} bundle exec jekyll build --profile -t -p /maps/_plugins/gems
   
