73c4cba576a05d850063e61d1bef9169f6961ed0
eQuantum <alzheimermom@gmail.com>
2024-05-02 09:43:06 +0700

    Update gist.sh

--- _plugins/scripts/gist.sh
+++ _plugins/scripts/gist.sh
@@ -31,6 +31,6 @@ gh gist clone e9832026b5b78f694e4ad22c3eb6c3ef /tmp/gistdir/exponentiation/span1
 
 find /tmp/workdir/identition -type f -name "*.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
 find /tmp/workdir/exponentiation -type f -name "*.md" -prune -exec sh -c 'mv -f "$1" "${1%/*}/README.md"' sh {} \;
-find /tmp/gistdir -type d -name "span17" -prune -exec sh -c 'rm -rf "$1/README.md" && cp -R /tmp/wikidir/* "$1"' sh {} \;
+find /tmp/gistdir -type d -name "span16" -prune -exec sh -c 'rm -rf "$1/README.md" && cp -R /tmp/wikidir/* "$1"' sh {} \;
 find /tmp/gistdir -type d -name .git -prune -exec rm -rf {} \; && find /tmp/gistdir -type f -name "README.md" -exec rm -rf {} \;
 find /tmp/gistdir -type f -name "*zone.md" -prune -exec sh -c 'mkdir --parent "${1%_*}" && mv -f "$1" "${1%_*}/README.md"' sh {} \;
