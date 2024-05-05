540688249477365b12ec233b9a15761542e894ed
eQuantum <alzheimermom@gmail.com>
2024-05-05 14:21:47 +0700

    Update script.js

--- _includes/extra/script.js
+++ _includes/extra/script.js
@@ -2,8 +2,5 @@
 $('.external-link').unbind('click');
 $(document.links).filter(function() {
   return this.hostname != window.location.hostname; 
-  //Array = {"eq19.com", "github.io", window.location.hostname};
-  //return Array.ToString.indexOf(this.hostname) === -1;
-  //return this.hostname != window.location.hostname && this.hostname.indexOf("github.io") === -1; 
 }).attr('target', '_blank')
 
