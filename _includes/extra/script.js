bf6c6ec45d005eab6a45e8dad91d7268aece1143
eQuantum <alzheimermom@gmail.com>
2024-05-05 14:14:44 +0700

    Update script.js

--- _includes/extra/script.js
+++ _includes/extra/script.js
@@ -1,4 +1,4 @@
-/* set links which point outside */
+/* set _blank for outside links */
 $('.external-link').unbind('click');
 $(document.links).filter(function() {
   return this.hostname != window.location.hostname; 
