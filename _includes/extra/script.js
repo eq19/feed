33a96a7004bfd82ca1319fdd8438e0c612f3c799
eQuantum <alzheimermom@gmail.com>
2024-05-05 14:53:35 +0700

    Update script.js

--- _includes/extra/script.js
+++ _includes/extra/script.js
@@ -1,6 +1,6 @@
 /* set _blank for outside links */
 $('.external-link').unbind('click');
 $(document.links).filter(function() {
-  return this.hostname != window.location.hostname && not this.hostname.Contains("github.io"); 
+  return this.hostname != window.location.hostname && this.hostname.indexOf("github.io") === -1; 
 }).attr('target', '_blank')
 
