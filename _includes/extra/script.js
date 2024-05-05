2140e0a54eb9eaa62464e287058924e80aec15cd
eQuantum <alzheimermom@gmail.com>
2024-05-05 13:26:01 +0700

    Update script.js

--- _includes/extra/script.js
+++ _includes/extra/script.js
@@ -3,6 +3,6 @@ $('.external-link').unbind('click');
 $(document.links).filter(function() {
   Array = {"eq19.com", "github.io"}
   //return this.hostname != window.location.hostname; 
-  return not Array.Find(arr,Function (Strr) Strr.ToString.Contains(this.hostname))
+  return Array.Find(arr,Function (Strr) Strr.ToString.Contains(this.hostname)) === -1;
 }).attr('target', '_blank')
 
