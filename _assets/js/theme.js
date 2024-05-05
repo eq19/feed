8855db0e84d7fa1c548a3e74ff50eda226b78886
eQuantum <alzheimermom@gmail.com>
2024-05-05 12:12:51 +0700

    Update theme.js

--- _assets/js/theme.js
+++ _assets/js/theme.js
@@ -285,9 +285,3 @@ if ("serviceWorker" in navigator) {
 } else {
   debug("Service Worker not supported!");
 }
-
-/* set links which point outside */
-$('.external-link').unbind('click');
-$(document.links).filter(function() {
-  return this.hostname != window.location.hostname;
-}).attr('target', '_blank')
