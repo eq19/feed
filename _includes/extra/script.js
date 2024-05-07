6281158bb47b8beb3dee532392e7204491b2d7f5
eQuantum <alzheimermom@gmail.com>
2024-05-07 13:02:02 +0700

    Update script.js

--- _includes/extra/script.js
+++ _includes/extra/script.js
@@ -1,6 +1,10 @@
 /* set _blank for outside links */
 $('.external-link').unbind('click');
 $(document.links).filter(function() {
-  return (this.hostname != window.location.hostname && this.hostname.indexOf("eq19.com") === -1 && this.hostname.indexOf("github.io") === -1); 
+  var cond = 0;
+  cond += (this.hostname != window.location.hostname)? 1: 0;
+  cond += (this.hostname.indexOf("eq19.com") === -1)? 1: 0;
+  cond += (this.hostname.indexOf("github.io") === -1)? 1: 0; 
+  return cond == 3;
 }).attr('target', '_blank')
 
