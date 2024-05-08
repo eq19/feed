831db42681451b46c834ae8035861de3a165a4ed
eQuantum <alzheimermom@gmail.com>
2024-05-09 03:27:59 +0700

    Update entrypoint.sh

--- _plugins/scripts/entrypoint.sh
+++ _plugins/scripts/entrypoint.sh
@@ -7,7 +7,7 @@ set_target() {
   # Get Structure
     if [[ $2 == *"github.io"* ]]; then
     [[ -n "$CELL" ]] && SPIN=$(( CELL * 7 ))
-    pinned_repos.rb ${OWNER} public | yq eval -P | sed "s/ /, /g" > /tmp/pinned_repo
+    pinned_repos.rb "eq19" public | yq eval -P | sed "s/ /, /g" > /tmp/pinned_repo
     IFS=', '; array=($(cat /tmp/pinned_repo))
   else
     gh api -H "${HEADER}" /user/orgs  --jq '.[].login' | sort -uf | yq eval -P | sed "s/ /, /g" > /tmp/user_orgs
