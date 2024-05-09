d46f6fb44ae05b8de021bc0d17cfbc820d25b1ab
eQuantum <alzheimermom@gmail.com>
2024-05-09 21:38:43 +0700

    Update entrypoint.sh

--- _plugins/scripts/entrypoint.sh
+++ _plugins/scripts/entrypoint.sh
@@ -46,7 +46,7 @@ set_target() {
   [[ -z "$SPIN" ]] && if [[ "$1" != "$2" ]]; then SPIN=0; else SPIN=13; fi
   if [[ -n "$CELL" ]]; then
     SPANPLUS=$(($SPAN + 1))
-    if [[ "${CELL}" == "0" )); then MOD = 7; else MOD = 13; fi
+    if [[ "${CELL}" == "0" ]]; then MOD = 7; else MOD = 13; fi
     if (( $SPANPLUS == $MOD )); then 
       SPANPLUS=0
       CELLPLUS=$(($CELL + 1))
