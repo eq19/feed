1999aba531f54bd6f4bd2be3ce5b9a92d8a7fba4
eQuantum <8466209+eq19@users.noreply.github.com>
2024-07-03 00:30:26 +0700

    Update entrypoint.sh

--- _plugins/scripts/entrypoint.sh
+++ _plugins/scripts/entrypoint.sh
@@ -118,7 +118,7 @@ CREDENTIAL=${INPUT_TOKEN}
 [[ "${OWNER}" != "${USER}" ]] && CREDENTIAL=${INPUT_OWNER}
 REMOTE_REPO="https://${ACTOR}:${CREDENTIAL}@github.com/${OWNER}/$1.git"
 git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
-git add . && git commit -m "jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
+#git add . && git commit -m "jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
 
 }
 
