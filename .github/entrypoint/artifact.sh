a3fe3374f60e89c08342177be017fd92452cc262
eq19 <eq19@users.noreply.github.com>
2025-06-16 08:55:12 +0000

    update workflows

--- .github/entrypoint/artifact.sh
+++ .github/entrypoint/artifact.sh
@@ -55,7 +55,15 @@ set_target() {
   fi
   
   # Generate id from the Structure
-  [[ -z "$SPIN" ]] && if [[ "$1" != "$2" ]]; then SPIN=0; else SPIN=13; fi
+  if [[ -z "$SPIN" ]]; then
+    if [[ "$1" != "$2" ]]; then
+echo "to be corrected as $1 != $2"
+      SPIN=-1
+    else
+      SPIN=13
+    fi
+  fi
+
   if [[ -n "$CELL" ]]; then
     SPANPLUS=$(($SPAN + 1))
     if (( $CELL == 0 )); then MOD=7; else MOD=13; fi
@@ -79,8 +87,15 @@ set_target() {
 jekyll_build() {
   
   [[ $1 == *"github.io"* ]] && OWNER=$2
-  if [[ $1 != "eq19.github.io" ]]; then SITEID=$(( $3 + 2 )); else SITEID=1; fi
-  
+
+  if [[ $1 == "Chetabahana.github.io" ]]; then
+    SITEID=1
+  elif [[ $1 == "eq19.github.io" ]]; then
+    SITEID=170
+  else
+    SITEID=$(( $3 + 2 ))
+  fi
+
   if  [[ "${OWNER}" == "eq19" ]]; then
     sed -i "1s|^|description: An attempt to discover the Final Theory\n\n|" ${RUNNER_TEMP}/_config.yml
   else
